9bda80978ccc095506d78530b61085fdfc27e114
EgonOlsen <info@jpct.net>
2017-02-28 22:26:27 +0100

    Initial

---
+++ src/main/java/de/sixtyfour/rage/CbmViewEngine.java
@@ -0,0 +1,59 @@
+package de.sixtyfour.rage;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.servlet.ServletException;
+
+import com.sixtyfour.templating.Template;
+import com.sixtyfour.templating.TemplateManager;
+
+import de.skaliant.wax.core.ViewEngine;
+import de.skaliant.wax.core.model.Call;
+import de.skaliant.wax.core.model.ViewEngineConfig;
+
+/**
+ * @author EgonOlsen
+ *
+ */
+public class CbmViewEngine implements ViewEngine {
+	private ViewEngineConfig info = new ViewEngineConfig();
+
+	public CbmViewEngine() {
+		info.setLocation("/WEB-INF/views/");
+		info.setSuffix(".cbm");
+	}
+
+	public ViewEngineConfig getConfig() {
+		return info;
+	}
+
+	public void render(String view, Call call) throws ServletException, IOException {
+		StringBuilder sb = new StringBuilder();
+		String path = null;
+		String viewLocation = info.getLocation();
+		String viewSuffix = info.getSuffix();
+
+		if (!view.startsWith("/")) {
+			sb.append(viewLocation);
+		}
+		sb.append(view);
+		if (!view.endsWith(".cbm")) {
+			sb.append(viewSuffix);
+		}
+		path = sb.toString();
+
+		path = call.getRealPath(path);
+		Template tmpl = TemplateManager.getInstance().getTemplate(path);
+		Map<String, Object> vars = call.getRequestScope().getNames().stream()
+				.collect(Collectors.toMap(Function.identity(), x -> call.getRequestScope().get(x)));
+		vars.putAll(call.getSessionScope().getNames().stream()
+				.collect(Collectors.toMap(Function.identity(), x -> call.getSessionScope().get(x))));
+		tmpl.setVariablesWithType(vars);
+
+		String res = tmpl.process();
+		call.getResponse().getWriter().println(res);
+	}
+}
---
+++ src/main/java/de/sixtyfour/rage/controllers/BeerController.java
@@ -0,0 +1,35 @@
+package de.sixtyfour.rage.controllers;
+
+import de.skaliant.wax.app.ControllerBase;
+
+/**
+ * @author EgonOlsen
+ *
+ */
+public class BeerController extends ControllerBase {
+
+	private int max = 99;
+
+	public void setMax(int max) {
+		this.max = max;
+	}
+
+	public int getMax() {
+		return this.max;
+	}
+
+	public String basic() {
+		setMaxValue();
+		return "basic_beer";
+	}
+
+	public String asm() {
+		setMaxValue();
+		return "asm_beer";
+	}
+
+	private void setMaxValue() {
+		this.request.setAttribute("mx", Math.max(1, Math.min(99, max)));
+	}
+
+}
